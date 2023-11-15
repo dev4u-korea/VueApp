4c2f0d454c60f605b547af8360e41815abeb88ff
dev4u-korea <moonsun4ux@gmail.com>
2023-11-15 12:02:56 +0900

    Init VueApp

---
+++ src/main/java/com/hecto/vueapp/VueAppApplication.java
@@ -0,0 +1,13 @@
+package com.hecto.vueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hecto/vueapp/VueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecto.vueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
