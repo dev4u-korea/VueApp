a6c6dd1679abe0073dea6c089f5cc9ef7d6d872e
moonsun <moonsun4ux@gmail.com>
2023-11-17 21:00:57 +0900

    vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         proxy: {
             '/api': {
                 // '/api' 로 들어오면 포트 8081(스프링 서버)로 보낸다
-                target: 'http://localhost:8081',
+                target: 'http://localhost:8080',
                 changeOrigin: true // cross origin 허용
             }
         }
