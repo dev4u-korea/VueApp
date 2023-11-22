fe593cf9114286d5af90693d227018e9d76a60c1
dev4u-korea <moonsun4ux@gmail.com>
2023-11-22 13:24:52 +0900

    snow sample init

--- frontend/public/snow.js
+++ frontend/public/snow.js
@@ -1,39 +1,41 @@
+"use strict";
+
 (function() {
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+    let requestAnimationFrame = window.requestAnimationFrame ||  // window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
     function(callback) {
         window.setTimeout(callback, 1000 / 60);
     };
+
     window.requestAnimationFrame = requestAnimationFrame;
 })();
 
 
-var flakes = [],
+let flakes = [],
     canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     flakeCount = 200,
     mX = -100,
     mY = -100
-
     canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+    canvas.height = window.innerHeight;
 
 function snow() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < flakeCount; i++) {
-        var flake = flakes[i],
+    for (let i = 0; i < flakeCount; i++) {
+        let flake = flakes[i],
             x = mX,
             y = mY,
             minDist = 150,
             x2 = flake.x,
             y2 = flake.y;
 
-        var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
-            dx = x2 - x,
-            dy = y2 - y;
+        let dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y));
+           // dx = x2 - x,
+           // dy = y2 - y;
 
         if (dist < minDist) {
-            var force = minDist / (dist * dist),
+            let force = minDist / (dist * dist),
                 xcomp = (x - x2) / dist,
                 ycomp = (y - y2) / dist,
                 deltaV = force / 2;
@@ -67,24 +69,24 @@ function snow() {
         ctx.fill();
     }
     requestAnimationFrame(snow);
-};
+}
 
 function reset(flake) {
     flake.x = Math.floor(Math.random() * canvas.width);
     flake.y = 0;
     flake.size = (Math.random() * 3) + 2;
-    flake.speed = (Math.random() * 1) + 0.5;
+    flake.speed = (Math.random() ) + 0.5;
     flake.velY = flake.speed;
     flake.velX = 0;
     flake.opacity = (Math.random() * 0.5) + 0.3;
 }
 
 function init() {
-    for (var i = 0; i < flakeCount; i++) {
-        var x = Math.floor(Math.random() * canvas.width),
+    for (let i = 0; i < flakeCount; i++) {
+        let x = Math.floor(Math.random() * canvas.width),
             y = Math.floor(Math.random() * canvas.height),
             size = (Math.random() * 3) + 2,
-            speed = (Math.random() * 1) + 0.5,
+            speed = (Math.random()) + 0.5,
             opacity = (Math.random() * 0.5) + 0.3;
 
         flakes.push({
@@ -102,11 +104,11 @@ function init() {
     }
 
     snow();
-};
+}
 
 canvas.addEventListener("mousemove", function(e) {
-    mX = e.clientX,
-    mY = e.clientY
-});
+    mX = e.clientX;
+    mY = e.clientY;
+})
 
 init();
